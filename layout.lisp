2f55f7977496b37b65d10f18f10b652cd698cf36
Shinmera <shinmera@tymoon.eu>
2019-11-20 12:47:50 +0100

    Don't automatically constrain visibility in layouts, as it prevents popups and such from working as expected.

--- layout.lisp
+++ layout.lisp
@@ -110,9 +110,8 @@
     (register element renderer)))
 
 (defmethod render ((renderer renderer) (layout layout))
-  (with-constrained-visibility ((bounds layout) renderer)
-    (do-elements (element layout)
-      (render renderer element))))
+  (do-elements (element layout)
+    (render renderer element)))
 
 (defmethod maybe-render ((renderer renderer) (layout layout))
   (do-elements (element layout)
