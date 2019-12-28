b9b98f8084b913e336677787fdc01ce6045fc715
Shinmera <shinmera@tymoon.eu>
2019-12-28 19:48:00 +0100

    Inspector structure

---
+++ structures/inspector.lisp
@@ -0,0 +1,62 @@
+#|
+ This file is a part of Alloy
+ (c) 2019 Shirakumo http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.shirakumo.alloy)
+
+(defclass inspector (structure)
+  ((object :initarg :object :initform NIL)
+   (class :initarg :class)
+   (slots :initarg :slots :initform :all)))
+
+(defun find-slot (name class)
+  (find name (c2mop:class-slots class) :key #'c2mop:slot-definition-name))
+
+(defun update-inspector-slots (inspector)
+  (let* ((layout (layout-element inspector))
+         (focus (focus-element inspector))
+         (object (slot-value inspector 'object))
+         (class (if (slot-boundp inspector 'class)
+                    (slot-value inspector 'class)
+                    (class-of object)))
+         (slots (slot-value inspector 'slots)))
+    (case slots
+      (:all
+       (setf slots (mapcar #'c2mop:slot-definition-name (c2mop:class-slots class))))
+      (:direct
+       (setf slots (mapcar #'c2mop:slot-definition-name (c2mop:class-direct-slots class)))))
+    (dolist (slot slots)
+      (with-simple-restart (continue "Ignore the slot ~a" slot)
+        (destructuring-bind (name &optional type &rest initargs) (if (listp slot) slot (list slot))
+          (when (slot-boundp object name)
+            (etypecase type
+              (class)
+              (null
+               (let ((slot-type (c2mop:slot-definition-type (find-slot name class))))
+                 (setf type (cond ((or (eq slot-type NIL) (eq slot-type T))
+                                   (class-of (slot-value object name)))
+                                  ((symbolp slot-type)
+                                   (find-class slot-type))
+                                  (T
+                                   ;; KLUDGE: This is /bad/
+                                   T)))))
+              (symbol
+               (setf type (find-class type))))
+            (let ((component (apply #'represent-with
+                                    (component-class-for-object (c2mop:class-prototype type))
+                                    (make-instance 'slot-data :object object :slot name)
+                                    initargs)))
+              (enter (string name) layout)
+              (enter component layout)
+              (enter component focus))))))))
+
+(defmethod initialize-instance :after ((structure inspector) &key focus-parent layout-parent)
+  (let ((layout (make-instance 'grid-layout :col-sizes '(100 T) :row-sizes '(30) :layout-parent layout-parent))
+        (focus (make-instance 'focus-list :focus-parent focus-parent)))
+    (finish-structure structure layout focus)
+    (update-inspector-slots structure)))
+
+(defmethod reinitialize-instance :after ((structure inspector) &key)
+  (update-inspector-slots structure))
