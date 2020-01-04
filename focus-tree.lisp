06eb1156f01778f7fd959c521a3c62762b5a4b0b
Shinmera <shinmera@tymoon.eu>
2020-01-04 13:44:47 +0100

    Focus next no tab by default

--- focus-tree.lisp
+++ focus-tree.lisp
@@ -206,6 +206,11 @@
   (when (and index (focused chain))
     (setf (slot-value chain 'index) (element-index (focused chain) chain))))
 
+(defmethod handle ((event key-up) (chain focus-chain))
+  (case (key event)
+    (:tab (focus-next chain))
+    (T (call-next-method))))
+
 (defmethod handle ((event focus-next) (chain focus-chain))
   (focus-next chain))
 
