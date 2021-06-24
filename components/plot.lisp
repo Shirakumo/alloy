85dc6c6ec36777f25f4013ecf0690f1d7c067f6c
Shinmera <shinmera@tymoon.eu>
2021-06-24 18:08:19 +0200

    Cache plot points.

--- components/plot.lisp
+++ components/plot.lisp
@@ -8,40 +8,53 @@
 
 (defclass plot (value-component)
   ((x-range :initarg :x-range :initform T :accessor x-range)
-   (y-range :initarg :y-range :initform T :accessor y-range)))
+   (y-range :initarg :y-range :initform T :accessor y-range)
+   (plot-points :initform #() :accessor plot-points)))
 
 (defmethod (setf x-range) :after (value (plot plot))
-  (mark-for-render plot))
+  (mark-for-render plot)
+  (recompute-plot-points plot))
 
 (defmethod (setf y-range) :after (value (plot plot))
-  (mark-for-render plot))
+  (mark-for-render plot)
+  (recompute-plot-points plot))
 
 (defmethod suggest-bounds (extent (plot plot))
   (extent (x extent) (y extent) (w extent) (umax (un 400) (h extent))))
+
+(defmethod value-changed ((plot plot))
+  (call-next-method)
+  (recompute-plot-points plot))
+
+(defun recompute-plot-points (plot)
+  (let ((x-range (x-range plot))
+        (y-range (y-range plot))
+        (data (value plot))
+        (extent (bounds plot))
+        xmin xmax ymin ymax)
+    (etypecase x-range
+      (cons (setf xmin (car x-range)
+                  xmax (cdr x-range)))
+      ((eql T) (setf xmin 0
+                     xmax (length data))))
+    (etypecase y-range
+      (cons (setf ymin (car y-range)
+                  ymax (cdr y-range)))
+      ((eql T) (setf ymin (loop for p across data minimize p)
+                     ymax (loop for p across data maximize p))))
+    (let* ((x-scale (/ (max 1 (pxw extent)) (- xmax xmin)))
+           (y-scale (/ (max 1 (pxh extent)) (- ymax ymin)))
+           (length (max (- xmax xmin)))
+           (points (plot-points plot)))
+      (when (/= length (length points))
+        (setf (plot-points plot) (setf points (make-array length))))
+      (loop for i from 0 below (length points)
+            for x from (/ x-scale 2) by x-scale
+            for p = (aref data (+ i xmin))
+            for y = (* (- p ymin -0.5) y-scale)
+            do (setf (aref points i) (px-point x y))))))
 
 (defmethod render :around ((renderer renderer) (component plot))
   ;; Ensures that a plotted line doesn't leave the bounds of the plot
   (with-constrained-visibility ((bounds component) renderer)
     (call-next-method)))
-
-(defmethod plot-points ((plot plot))
-  (let ((x-range (x-range plot))
-        (y-range (y-range plot))
-        (data (value plot))
-        (extent (bounds plot)))
-    (when (eql T x-range)
-      (setf x-range (cons 0 (length data))))
-    (when (eql T y-range)
-      (setf y-range (cons (loop for p across data minimize p)
-                          (loop for p across data maximize p))))
-    (destructuring-bind (xmin . xmax) x-range
-      (destructuring-bind (ymin . ymax) y-range
-        (let ((x-scale (/ (max 1 (pxw extent)) (- xmax xmin)))
-              (y-scale (/ (max 1 (pxh extent)) (- ymax ymin)))
-              (points (make-array (max 0 (- xmax xmin)))))
-          (loop for i from 0 below (length points)
-                for x from (/ x-scale 2) by x-scale
-                for p = (aref data (+ i xmin))
-                for y = (* (- p ymin -0.5) y-scale)
-                do (setf (aref points i) (px-point x y)))
-          points)))))
