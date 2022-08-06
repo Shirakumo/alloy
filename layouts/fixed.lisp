5fd90ff299f5f87c2dcb8f08a0b2a91fe0eac997
Shinmera <shinmera@tymoon.eu>
2022-08-06 14:22:25 +0200

    Don't move fixed layout as now with local coords it would offset wrong.

--- layouts/fixed.lisp
+++ layouts/fixed.lisp
@@ -13,19 +13,7 @@
   (do-elements (element layout)
     (setf (bounds element) (bounds element))))
 
-(defmethod notice-size ((element layout-element) (layout fixed-layout))
-  ;; Calculate max bound
-  (cond ((= 1 (element-count layout))
-         (setf (slot-value layout 'bounds) (bounds element)))
-        (T
-         (destructure-extent (:x lx :y ly :w lw :h lh :to-px T) (bounds layout)
-           (destructure-extent (:x ex :y ey :w ew :h eh :to-px T) (bounds element)
-             (let ((l (min lx ex))
-                   (b (min ly ey))
-                   (r (max (+ lx lw) (+ ex ew)))
-                   (u (max (+ ly lh) (+ ey eh))))
-               (setf (slot-value layout 'bounds)
-                     (px-extent l b (- r l) (- u b)))))))))
+(defmethod notice-size ((element layout-element) (layout fixed-layout)))
 
 (defmethod suggest-size (size (layout fixed-layout))
   size)
