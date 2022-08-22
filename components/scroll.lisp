0fe627a3e5f839a80ecd5b223575b393bf6bbfbc
Shinmera <shinmera@tymoon.eu>
2022-08-22 15:17:20 +0200

    Fix scrolling direction

--- components/scroll.lisp
+++ components/scroll.lisp
@@ -18,32 +18,32 @@
   ((orientation :initform :horizontal)))
 
 (defmethod value ((scrollbar x-scrollbar))
-  (unit-value (x (offset (data scrollbar)))))
+  (- (unit-value (x (offset (data scrollbar))))))
 
 (defmethod (setf value) (value (scrollbar x-scrollbar))
   (setf (offset (data scrollbar))
-        (px-point value (y (offset (data scrollbar))))))
+        (px-point (- value) (y (offset (data scrollbar))))))
 
 (defmethod range ((scrollbar x-scrollbar))
   (let ((layout (data scrollbar)))
     (if (inner layout)
         (with-unit-parent layout
-          (cons (min 0 (- (pxw (bounds layout)) (pxw (bounds (inner layout))))) 0))
+          (cons 0 (max 0 (- (pxw (bounds (inner layout))) (pxw (bounds layout))))))
         '(0 . 0))))
 
 (defclass y-scrollbar (scrollbar)
   ((orientation :initform :vertical)))
 
 (defmethod value ((scrollbar y-scrollbar))
-  (unit-value (y (offset (data scrollbar)))))
+  (- (unit-value (y (offset (data scrollbar))))))
 
 (defmethod (setf value) (value (scrollbar y-scrollbar))
   (setf (offset (data scrollbar))
-        (px-point (x (offset (data scrollbar))) value)))
+        (px-point (x (offset (data scrollbar))) (- value))))
 
 (defmethod range ((scrollbar y-scrollbar))
   (let ((layout (data scrollbar)))
     (if (inner layout)
         (with-unit-parent layout
-          (cons (min 0 (- (pxh (bounds layout)) (pxh (bounds (inner layout))))) 0))
+          (cons 0 (max 0 (- (pxh (bounds (inner layout))) (pxh (bounds layout))))))
         '(0 . 0))))
