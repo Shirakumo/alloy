7620558d663ba5ffce4e8f5dba0b11016d9a8199
Shinmera <shinmera@tymoon.eu>
2023-05-16 16:03:36 +0200

    pies in line circles

--- renderers/opengl/renderer.lisp
+++ renderers/opengl/renderer.lisp
@@ -170,20 +170,41 @@ void main(){
                  "
 layout (location=0) in vec2 pos;
 uniform mat3 transform;
+uniform float start_angle;
+uniform float end_angle;
 out vec2 uv;
+out vec2 c;
+
+#define PI_2 1.5707963267948966
 
 void main(){
   uv = pos-0.5;
+  float start = start_angle-PI_2;
+  float aperture = abs(end_angle-start_angle)*0.5;
+  if(end_angle < start_angle){
+    start += -2*(aperture+PI_2);
+    aperture -= 2*PI_2;
+  }
+
+  c = vec2(sin(aperture), cos(aperture));
+  float cstart = cos(aperture+start);
+  float sstart = sin(aperture+start);
+  uv = mat2(cstart,-sstart,
+            sstart, cstart)*uv;
   gl_Position = vec4(transform*vec3(pos, 1.0), 1.0);
 }"
                  "
 uniform vec4 color;
 uniform float line_width = 3.0;
 in vec2 uv;
+in vec2 c;
 out vec4 out_color;
 
 void main(){
-  float sdf = max(length(uv)-0.5, (0.5-line_width)-length(uv));
+  vec2 p = vec2(abs(uv.x), uv.y);
+  float l = length(p)-0.5;
+  float m = length(p-c*clamp(dot(p,c),0.0,0.5));
+  float sdf = max(max(l,m*sign(c.y*p.x-c.x*p.y)), (0.5-line_width)-length(uv));
   float dsdf = fwidth(sdf)*0.5;
   sdf = smoothstep(dsdf, -dsdf, sdf);
   out_color = vec4(color.rgb, color.a*sdf);
@@ -491,6 +512,8 @@ void main(){
       (simple:translate renderer (simple:bounds shape))
       (simple:scale renderer (alloy:px-size w h))
       (setf (uniform shader "transform") (simple:transform-matrix renderer)))
+    (setf (uniform shader "start_angle") (simple:start-angle shape))
+    (setf (uniform shader "end_angle") (simple:end-angle shape))
     (setf (uniform shader "color") color)
     ;; KLUDGE: I don't think this is quite right yet but whatever.
     (setf (uniform shader "line_width") (/ (alloy:to-px (simple:line-width shape))
