42ccc5f058709e4b198eb2184e3926224c6c4b56
Shinmera <shinmera@tymoon.eu>
2024-04-24 19:41:27 +0200

    Delete the obsolete svg backend for now

--- renderers/svg/package.lisp
+++
@@ -1,7 +0,0 @@
-(defpackage #:org.shirakumo.alloy.renderers.svg
-  (:use #:cl)
-  (:local-nicknames
-   (#:alloy #:org.shirakumo.alloy)
-   (#:simple #:org.shirakumo.alloy.renderers.simple)
-   (#:colored #:org.shirakumo.alloy.colored))
-  (:export))
