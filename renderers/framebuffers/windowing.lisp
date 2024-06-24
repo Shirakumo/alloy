47f8852e071a050ef05f777f72e04f39be2a2dd0
Shinmera <shinmera@tymoon.eu>
2024-06-24 22:08:17 +0200

    Sketch out event handling

--- renderers/framebuffers/windowing.lisp
+++ renderers/framebuffers/windowing.lisp
@@ -28,6 +28,15 @@
 (defmethod make-window ((screen screen) &rest args &allow-other-keys)
   (apply #'make-instance 'window :screen screen args))
 
+(defmethod process-events ((screen screen))
+  (let ((to-delete ()))
+    (loop for window in (windows screen)
+          do (fb:process-events window)
+             (when (fb:close-requested-p window)
+               (push window to-delete)))
+    (when to-delete
+      (setf (windows screen) (set-difference (windows screen) to-delete)))))
+
 (defclass cursor (window:cursor)
   ((native :initarg :native :accessor native)
    (icon :initform :default :accessor icon :reader window:icon)))
@@ -41,13 +50,13 @@
           (symbol icon)))
   (setf (icon cursor) icon))
 
-(defclass window (fb:event-handler)
+(defclass window (alloy:ui fb:event-handler
+                  org.shirakumo.alloy.renderers.simple.presentations::default-look-and-feel)
   ((native :accessor native)
    (screen :initarg :screen :reader window:screen)
    (background-color :initform colors:white :accessor window:background-color)
    (icon :initform NIL :accessor icon :reader window:icon)
-   (cursor :reader window:cursor)
-   (bounds :initform (alloy:px-extent) :accessor alloy:bounds)))
+   (cursor :reader window:cursor)))
 
 (defmethod initialize-instance :after ((window window) &key)
   (setf (slot-value window 'cursor) (make-instance 'cursor :window native)))
@@ -63,9 +72,15 @@
 
 (defmethod window:move-to-back ((window window)))
 
+(defmethod alloy:bounds ((window window))
+  (destructuring-bind (w h) (fb:size (native window))
+    (destructuring-bind (x y) (fb:location (native window))
+      (alloy:px-extent x y w h))))
+
 (defmethod (setf alloy:bounds) (bounds (window window))
   (setf (fb:size (native window)) (list (alloy:pxw bounds) (alloy:pxh bounds)))
-  (setf (fb:location (native window)) (list (alloy:pxx bounds) (alloy:pxy bounds))))
+  (setf (fb:location (native window)) (list (alloy:pxx bounds) (alloy:pxy bounds)))
+  bounds)
 
 (defmethod window:max-size ((window window))
   (destructuring-bind (w h) (fb:maximum-size (native window))
@@ -140,33 +155,51 @@
 ;;;; Event translations
 (defmethod fb:window-moved ((window window) xpos ypos))
 
-(defmethod fb:window-resized ((window window) width height))
+(defmethod fb:window-resized ((window window) width height)
+  (alloy:notice-size (alloy:px-size width height) window))
 
-(defmethod fb:window-refreshed ((window window)))
+(defmethod fb:window-refreshed ((window window))
+  (alloy:render (window:screen window) window))
 
 (defmethod fb:window-focused ((window window) focused-p))
 
-(defmethod fb:window-iconified ((window window) iconified-p))
+(defmethod fb:window-iconified ((window window) iconified-p)
+  (alloy:handle (make-instance 'window:state :new-state (if iconified-p :iconified :normal)) window))
 
-(defmethod fb:window-maximized ((window window) maximized-p))
+(defmethod fb:window-maximized ((window window) maximized-p)
+  (alloy:handle (make-instance 'window:state :new-state (if iconified-p :maximized :normal)) window))
 
-(defmethod fb:window-closed ((window window)))
+(defmethod fb:window-closed ((window window))
+  (alloy:handle (make-instance 'window:close) window))
 
-(defmethod fb:mouse-button-changed ((window window) button action modifiers))
+(defmethod fb:mouse-button-changed ((window window) button action modifiers)
+  (let ((location (pointer-location window)))
+    (case action
+      (:press (alloy:handle (make-instance 'alloy:pointer-down :kind button :location location) window))
+      (:release (alloy:handle (make-instance 'alloy:pointer-up :kind button :location location) window)))))
 
-(defmethod fb:mouse-moved ((window window) xpos ypos))
+(defmethod fb:mouse-moved ((window window) xpos ypos)
+  (let ((location (pointer-location window)))
+    (alloy:handle (make-instance 'alloy:pointer-move :old-location location :location new) window)
+    (setf (pointer-location window) new)))
 
-(defmethod fb:mouse-entered ((window window) entered-p))
+(defmethod fb:mouse-entered ((window window) entered-p)
+  (alloy:handle (make-instance (if entered-p 'window:pointer-enter 'window:pointer-leave)) window))
 
-(defmethod fb:mouse-scrolled ((window window) xoffset yoffset))
+(defmethod fb:mouse-scrolled ((window window) xoffset yoffset)
+  (alloy:handle (make-instance 'alloy:scroll :dx xoffset :dy yoffset :location (pointer-location window)) window))
 
-(defmethod fb:key-changed ((window window) key scan-code action modifiers))
+(defmethod fb:key-changed ((window window) key scan-code action modifiers)
+  (alloy:handle (make-instance 'alloy:key-down :modifiers modifiers :key key :code scan-code) window))
 
-(defmethod fb:string-entered ((window window) string))
+(defmethod fb:string-entered ((window window) string)
+  (alloy:handle (make-instance 'alloy:text-event :text string) window))
 
-(defmethod fb:file-dropped ((window window) paths))
+(defmethod fb:file-dropped ((window window) paths)
+  (alloy:handle (make-instance 'alloy:drop-event :paths paths) window))
 
-(defmethod fb:content-scale-changed (window xscale yscale))
+(defmethod fb:content-scale-changed (window xscale yscale)
+  (alloy:handle (make-instance 'alloy:scale-changed) window))
 
 (defmethod fb:display-connected ((window window) display connected-p)
   (if connected-p
