8132796573fa01cf7771cc69732261ab58f75da6
Shinmera <shinmera@tymoon.eu>
2024-06-24 21:49:20 +0200

    Start implementing this

--- renderers/framebuffers/package.lisp
+++ renderers/framebuffers/package.lisp
@@ -3,7 +3,6 @@
   (:local-nicknames
    (#:alloy #:org.shirakumo.alloy)
    (#:simple #:org.shirakumo.alloy.renderers.simple)
-   (#:opengl #:org.shirakumo.alloy.renderers.opengl)
    (#:window #:org.shirakumo.alloy.windowing)
    (#:animation #:org.shirakumo.alloy.animation)
    (#:colored #:org.shirakumo.alloy.colored)
--- renderers/framebuffers/windowing.lisp
+++ renderers/framebuffers/windowing.lisp
@@ -1,76 +1,141 @@
 (in-package #:org.shirakumo.alloy.renderers.framebuffers)
 
 (defclass monitor (window:monitor)
-  ((native :accessor native)))
+  ((native :initarg :native :accessor native)))
 
-(defmethod alloy:bounds ((monitor monitor)))
+(defmethod alloy:bounds ((monitor monitor))
+  (destructuring-bind (w h) (fb:size (native monitor))
+    (destructuring-bind (x y) (fb:location (native monitor))
+      (alloy:px-extent x y w h))))
 
 (defclass screen (window:screen)
-  ())
+  ((monitors :initform () :accessor monitors :reader window:list-monitors)
+   (windows :initform () :accessor windows :reader window:list-windows)))
 
-(defmethod list-monitors ((screen screen)))
-
-(defmethod list-windows ((screen screen)))
+(defmethod initialize-instance :after ((screen screen) &key backend)
+  (fb:init backend)
+  (dolist (display (fb:list-displays))
+    (push (make-instance 'monitor :native display) (monitors screen))))
 
 (defmethod alloy:bounds ((screen screen)))
 
+(defmethod alloy:cursor ((screen screen))
+  :default)
+
+(defmethod (setf alloy:cursor) (value (screen screen))
+  value)
+
+(defmethod make-window ((screen screen) &rest args &allow-other-keys)
+  (apply #'make-instance 'window :screen screen args))
+
+(defclass cursor (window:cursor)
+  ((native :initarg :native :accessor native)
+   (icon :initform :default :accessor icon :reader window:icon)))
+
+(defmethod (setf window:icon) (icon (cursor cursor))
+  (setf (fb:cursor-icon (native cursor))
+        (etypecase icon
+          (simple:image (fb:make-icon (round (alloy:pxw (simple:size icon)))
+                                      (round (alloy:pxh (simple:size icon)))
+                                      (simple:data icon)))
+          (symbol icon)))
+  (setf (icon cursor) icon))
+
 (defclass window (fb:event-handler)
-  ((native :accessor native)))
+  ((native :accessor native)
+   (screen :initarg :screen :reader window:screen)
+   (background-color :initform colors:white :accessor window:background-color)
+   (icon :initform NIL :accessor icon :reader window:icon)
+   (cursor :reader window:cursor)
+   (bounds :initform (alloy:px-extent) :accessor alloy:bounds)))
 
-(defmethod make-window ((screen screen) &key title icon bounds min-size max-size
-
-                                             state decorated-p always-on-top-p
-                                             background-color
-                        &allow-other-keys))
+(defmethod initialize-instance :after ((window window) &key)
+  (setf (slot-value window 'cursor) (make-instance 'cursor :window native)))
 
 ;;;; Window control
-(defmethod close ((window window)))
+(defmethod window:close ((window window))
+  (fb:close (native window)))
 
-(defmethod notify ((window window)))
+(defmethod window:notify ((window window))
+  (fb:request-attention (native window)))
 
-(defmethod cursor ((window window)))
+(defmethod window:move-to-front ((window window)))
 
-(defmethod move-to-front ((window window)))
+(defmethod window:move-to-back ((window window)))
 
-(defmethod move-to-back ((window window)))
+(defmethod (setf alloy:bounds) (bounds (window window))
+  (setf (fb:size (native window)) (list (alloy:pxw bounds) (alloy:pxh bounds)))
+  (setf (fb:location (native window)) (list (alloy:pxx bounds) (alloy:pxy bounds))))
 
-(defmethod alloy:bounds ((window window)))
+(defmethod window:max-size ((window window))
+  (destructuring-bind (w h) (fb:maximum-size (native window))
+    (alloy:px-size (or w 0) (or h 0))))
 
-(defmethod (setf alloy:bounds) (bounds (window window)))
+(defmethod (setf window:max-size) (size (window window))
+  (if size
+      (setf (fb:maximum-size (native window)) (list (alloy:pxw size) (alloy:pxh size)))
+      (setf (fb:maximum-size (native window)) NIL)))
 
-(defmethod background-color ((window window)))
+(defmethod window:min-size ((window window))
+  (destructuring-bind (w h) (fb:minimum-size (native window))
+    (alloy:px-size (or w 0) (or h 0))))
 
-(defmethod (setf background-color) (color (window window)))
+(defmethod (setf window:min-size) (size (window window))
+  (if size
+      (setf (fb:minimum-size (native window)) (list (alloy:pxw size) (alloy:pxh size)))
+      (setf (fb:minimum-size (native window)) NIL)))
 
-(defmethod max-size ((window window)))
+(defmethod window:decorated-p ((window window))
+  (not (fb:borderless-p (native window))))
 
-(defmethod (setf max-size) (size (window window)))
+(defmethod (setf window:decorated-p) (decorated (window window))
+  (setf (fb:borderless-p (native window)) (not decorated))
+  decorated)
 
-(defmethod min-size ((window window)))
+(defmethod window:title ((window window))
+  (fb:title (native window)))
 
-(defmethod (setf min-size) (size (window window)))
+(defmethod (setf window:title) (title (window window))
+  (setf (fb:title (native window)) title))
 
-(defmethod decorated-p ((window window)))
+(defmethod (setf window:icon) ((icon null) (window window))
+  (setf (fb:icon (native window)) icon)
+  (setf (icon window) icon))
 
-(defmethod (setf decorated-p) (decorated (window window)))
+(defmethod (setf window:icon) ((icon simple:image) (window window))
+  (setf (fb:icon (native window)) (fb:make-icon (round (alloy:pxw (simple:size icon)))
+                                                (round (alloy:pxh (simple:size icon)))
+                                                (simple:data icon)))
+  (setf (icon window) icon))
 
-(defmethod title ((window window)))
+(defmethod window:always-on-top-p ((window window))
+  (fb:always-on-top-p (native window)))
 
-(defmethod (setf title) (title (window window)))
+(defmethod (setf window:always-on-top-p) (top (window window))
+  (setf (fb:always-on-top-p (native window)) top))
 
-(defmethod icon ((window/cursor window)))
+(defmethod window:state ((window window))
+  (cond ((fb:iconified-p (native window)) :minimized)
+        ((not (fb:visible-p (native window))) :hidden)
+        ((fb:fullscreen-p (native window)) :fullscreen)
+        ((fb:maximized-p (native window)) :maximized)
+        (T :normal)))
 
-(defmethod (setf icon) (icon (window window)))
+(defmethod (setf window:state) (state (window window))
+  (ecase state
+    (:minimized (setf (fb:minimized-p (native window)) T))
+    (:maximized (setf (fb:maximized-p (native window)) T))
+    (:fullscreen (setf (fb:fullscreen-p (native window)) T))
+    (:hidden (setf (fb:visible-p (native window)) NIL))
+    (:normal
+     (setf (fb:maximized-p (native window)) NIL)
+     (setf (fb:minimized-p (native window)) NIL)
+     (setf (fb:fullscreen-p (native window)) NIL)
+     (setf (fb:visible-p (native window)) T)))
+  state)
 
-(defmethod always-on-top-p ((window window)))
-
-(defmethod (setf always-on-top-p) (top (window window)))
-
-(defmethod state ((window window)))
-
-(defmethod (setf state) (state (window window)))
-
-(defmethod fullscreen ((window window) monitor))
+(defmethod window:fullscreen ((window window) monitor)
+  (setf (fb:fullscreen-p (native window)) monitor))
 
 ;;;; Event translations
 (defmethod fb:window-moved ((window window) xpos ypos))
@@ -103,4 +168,7 @@
 
 (defmethod fb:content-scale-changed (window xscale yscale))
 
-(defmethod fb:display-connected ((window window) display connected-p))
+(defmethod fb:display-connected ((window window) display connected-p)
+  (if connected-p
+      (push (make-instance 'monitor :native display) (displays (window:screen window)))
+      (setf (displays (window:screen window)) (remove display (displays (window:screen window)) :key #'native))))
