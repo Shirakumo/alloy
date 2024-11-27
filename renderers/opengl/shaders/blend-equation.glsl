a2f5404e69564b04a5250e140311ff4e992e2d22
Yukari Hafner <shinmera@tymoon.eu>
2024-11-27 09:48:51 +0100

    Enable GL_ARB_sample_shading

--- renderers/opengl/shaders/blend-equation.glsl
+++ renderers/opengl/shaders/blend-equation.glsl
@@ -1,4 +1,9 @@
 #extension GL_KHR_blend_equation_advanced : enable
 #ifdef GL_KHR_blend_equation_advanced
-layout(blend_support_all_equations) out;
+  // NOTE: Even though we don't use any of the gl_Sample* variables
+  //       The AMD driver on Windows complains about it being used
+  //       without this extension, so... we enable it explicitly.
+  //       Gotta love AMD!!!!!!!!
+  #extension GL_ARB_sample_shading : enable
+  layout(blend_support_all_equations) out;
 #endif
